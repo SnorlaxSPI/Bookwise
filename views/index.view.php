7d6b7d25837de63db81d60bef7907b4a1a2f43f9
SnorlaxSPI <aflinsjr@icloud.com>
2025-01-04 20:18:02 -0300

    WSE-009 - Criando nossas primeiras views

---
+++ views/index.view.php
@@ -0,0 +1,45 @@
+<form class="w-full flex space-x-2 mt-6">
+
+  <input
+
+    type="text"
+    class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-4 px-2"
+    placeholder="Pesquisar..." />
+
+  <button type="submit">🔎</button>
+
+</form>
+
+<!-- Lista de Livros -->
+
+<section class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+
+  <?php foreach ($livros as $livro) : ?>
+
+    <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+
+      <div class="flex">
+
+        <div class="w-1/3">imagem</div>
+
+        <div class="space-y-1">
+
+          <a href="/livro.php?id=<?= $livro['id'] ?>" class="font-semibold hover:underline"><?= $livro['titulo'] ?></a>
+          <div class="text-xs italic"><?= $livro['autor'] ?></div>
+          <div class="text-xs italic">⭐️⭐️⭐️⭐️⭐️ (3 Avaliações)</div>
+
+        </div>
+
+      </div>
+
+      <div class="text-sm mt-2">
+
+        <?= $livro['descricao'] ?>
+
+      </div>
+
+    </div>
+
+  <?php endforeach; ?>
+
+</section>
