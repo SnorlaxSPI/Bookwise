555aaeae6cc227d2cee161570da700aafaecea6e
Alessandro L <alessandro.lins@gruponewway.com.br>
2025-01-05 18:42:32 -0300

    WSE-010 - Criando os nossos controllers

--- controllers/livro.controller.php
+++ controllers/livro.controller.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'dados.php';
+
 $id = isset($_REQUEST['id']) ? $_REQUEST['id'] : 'Não informado';
 
 $filtrado = array_values(array_filter($livros, fn($livro) => $livro['id'] == $id));
@@ -8,4 +9,5 @@ $filtrado = array_values(array_filter($livros, fn($livro) => $livro['id'] == $id
 $livro = array_pop($filtrado);
 
 $view = "livro";
+
 require "views/template/app.php";
--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-require "dados.php";
-$view = "index";
-require "views/template/app.php";
+$controller = "index";
+//$view = "index";
+
+
+if (isset($_SERVER['PATH_INFO'])) {
+    $controller = str_replace("/", "", $_SERVER['PATH_INFO']);
+}
+
+if (file_exists("controllers/{$controller}.controller.php")) {
+    require "controllers/{$controller}.controller.php";
+} else {
+    echo "Controller not found.";
+}
+
+//require "views/template/app.php";
