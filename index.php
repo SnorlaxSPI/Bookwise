61a9d5daa8aa00dcdf8d94c3239b7645d10b26ee
Alessandro L <alessandro.lins@gruponewway.com.br>
2025-01-07 20:51:44 -0300

    WSE-011 - Criando um roteador

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
 
-$controller = "index";
-//$view = "index";
-
-
-if (isset($_SERVER['PATH_INFO'])) {
-    $controller = str_replace("/", "", $_SERVER['PATH_INFO']);
-}
-
-if (file_exists("controllers/{$controller}.controller.php")) {
-    require "controllers/{$controller}.controller.php";
-} else {
-    echo "Controller not found.";
-}
-
-//require "views/template/app.php";
+require 'routes.php';
---
+++ routes.php
@@ -0,0 +1,23 @@
+<?php
+
+function carregarController() {
+  $controller = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
+  
+  if(!$controller) $controller = 'index';
+
+ // if (isset($_SERVER['PATH_INFO'])) {
+ //     $controller = str_replace("/", "", $_SERVER['PATH_INFO']);
+ // }
+  
+  
+  if (!file_exists("controllers/{$controller}.controller.php")) {
+    http_response_code(404);
+    echo "Página não existe";
+    die();
+  }  
+  require "controllers/{$controller}.controller.php";
+}
+
+carregarController();
+
+?>
